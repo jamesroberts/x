5ab39d2760611f1dc79e4189d8a91617211a693d
James Roberts <jamesroberts.dev@gmail.com>
2021-07-18 23:45:48 +0200

    Adds post test

--- tests/test_x.py
+++ tests/test_x.py
@@ -1,5 +1,11 @@
 
-def test_app(client):
+def test_get(client):
     response = client.get('/get')
+    assert response.status_code == 200
+    assert response.get_data() is not None
+
+
+def test_post(client):
+    response = client.post('/post', data={"test": "data"})
     assert response.status_code == 200
     assert response.get_data() is not None
